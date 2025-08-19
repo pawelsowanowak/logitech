4f68f636cf4b38ea8984459c60205bafa20a3e88
pawelnow <pawel.8.nowak@gmail.com>
2025-08-19 07:42:45 +0200

    Add apk ui parser

---
+++ src/logitech/buggy_calc/helpers/exceptions.py
@@ -0,0 +1,6 @@
+class ResultNotFoundError(Exception):
+    """Raised when a calculation result cannot be found or retrieved."""
+
+
+class InvalidAppFieldError(Exception):
+    """Raised when an invalid app field is referenced."""
